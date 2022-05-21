c3cc95514de3b95c76fb19b15cfd92997d1e2757
Romalyy <1303roma01@gmail.com>
2022-05-21 21:14:29 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
-const galleryItem = document.querySelector('.gallery__item');
-const galleryLink = document.querySelector('.gallery__link');
-const galleryImage = document.querySelector('.gallery__image');
+
 
 const galleryMarkup = createGallery(galleryItems);
 
@@ -26,25 +24,66 @@ function createGallery(images) {
 }
 
 function galleryItemClick(e) {
-    if (!e.target.classList.contains('gallery__item')) {
-        return;
-    }
-    console.log(e.target.src);
-}
-
-// function openModal(e) {
-
-// }
-
-const instance = basicLightbox.create(`
-    <img src="${preview}" width="800" height="600">
+  if (!e.target.classList.contains('gallery__item')) {
+        return ;
+  }
+  const basicLightbox = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${original}" width="800" height="600">
 `)
 
-instance.show()
+  instance.show();
 
-// createGallery(galleryItems)
-
-
-// console.log(createGallery(galleryItems));
+}
 
 
+
+// import { galleryItems } from "./gallery-items.js";
+
+// const galleryRefs = document.querySelector(".gallery");
+// galleryRefs.addEventListener("click", onShowBigImage);
+
+// (function createMarkup() {
+//   const itemMarkup = galleryItems
+//     .map(({ original, preview, description }) => {
+//       return `
+//     <div class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//             <img
+//                 class="gallery__image"
+//                 src="${preview}"
+//                 data-source="${original}"
+//                 alt="${description}"
+//             />
+//         </a>
+//     </div>
+//     `;
+//     })
+//     .join("");
+//   galleryRefs.insertAdjacentHTML("beforeend", itemMarkup);
+// })();
+
+// function onShowBigImage(e) {
+//   e.preventDefault();
+//   if (!e.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+
+//   let bigImageSrc = e.target.dataset.source;
+
+//   const modal = basicLightbox.create(
+//     `<img src="${bigImageSrc}" width="800" height="600">`
+//   );
+//   modal.show();
+
+//   if (modal.visible()) {
+//     window.addEventListener("keydown", onPressKeyESC);
+//   }
+
+//   function onPressKeyESC(e) {
+//     if (e.code === "Escape") {
+//       modal.close();
+//       window.removeEventListener("keydown", onPressKeyESC);
+//     }
+//   }
+// }
